68e3a9a167fc033e7d7cddc4b8e2455157d9b5b6
Beast <91755865+TheHellRuler@users.noreply.github.com>
2024-04-25 13:08:07 +0530

    Better format !?

--- colab_leecher/downlader/ytdl.py
+++ colab_leecher/downlader/ytdl.py
@@ -96,7 +96,7 @@ def YouTubeDL(url):
             logging.info(d)
 
     ydl_opts = {
-        "format": "best",
+        "format": "bv*+ba/b",
         "allow_multiple_video_streams": True,
         "allow_multiple_audio_streams": True,
         "writethumbnail": True,
